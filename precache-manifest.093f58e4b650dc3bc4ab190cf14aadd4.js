e33b2db89ad652daa6f3b34ad050bbe3c94ee256
dexagod <Ruben.Dedecker@Ugent.be>
2020-09-24 23:25:09 +0200

    Updates

---
+++ precache-manifest.093f58e4b650dc3bc4ab190cf14aadd4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dabc61edd68bd1237a116ca10739b584",
+    "url": "/SEMIC-solid-demo/index.html"
+  },
+  {
+    "revision": "b5eef737f1e6957d5dd3",
+    "url": "/SEMIC-solid-demo/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9f3cdf3e057c320bebdd",
+    "url": "/SEMIC-solid-demo/static/css/main.234796bf.chunk.css"
+  },
+  {
+    "revision": "b5eef737f1e6957d5dd3",
+    "url": "/SEMIC-solid-demo/static/js/2.e9bccfe2.chunk.js"
+  },
+  {
+    "revision": "b710c11273a0a98dd8568b881e4740e2",
+    "url": "/SEMIC-solid-demo/static/js/2.e9bccfe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f3cdf3e057c320bebdd",
+    "url": "/SEMIC-solid-demo/static/js/main.e98f2295.chunk.js"
+  },
+  {
+    "revision": "c31dafc3207fc0fe0fde",
+    "url": "/SEMIC-solid-demo/static/js/runtime-main.6bc646be.js"
+  },
+  {
+    "revision": "526592425a7bd2a4594b40890e4672db",
+    "url": "/SEMIC-solid-demo/static/media/solid-emblem.52659242.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7fa6864ebb368de904b0080441a0bf37.js"
+  "/SEMIC-solid-demo/precache-manifest.093f58e4b650dc3bc4ab190cf14aadd4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SEMIC-solid-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
